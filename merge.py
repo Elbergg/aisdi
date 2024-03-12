0b84ae924fd209e2b22127eb5c57c6b635111495
Elbergg <xxtomsxx89@gmail.com>
2024-03-12 22:29:48 +0100

    fuck me

--- merge.py
+++ merge.py
@@ -7,24 +7,34 @@ import copy
 def merge(list, p, q, r):
     n1 = q - p + 1
     n2 = r - q
-    L = []
-    R = []
+    L = [0] * (n1)
+    R = [0] * (n2)
     for i in range(0, n1):
-        L.append(list[p + i])
+        L[i] = list[p + i]
     for j in range(0, n2):
-        R.append(list[q + j + 1])
-    L.append(float("inf"))
-    R.append(float("inf"))
+        R[j] = list[q + j + 1]
+    # L[n1] = float("inf")
+    # R[n2] = float("inf")
     i = 0
     j = 0
-    for n in range(p, r + 1):
+    k = p
+    while i < n1 and j < n2:
         if L[i] <= R[j]:
-            list[n] = L[i]
+            list[k] = L[i]
             i += 1
         else:
-            list[n] = R[j]
+            list[k] = R[j]
             j += 1
-    return list
+        k += 1
+    while i < n1:
+        list[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        list[k] = R[j]
+        j += 1
+        k += 1
 
 
 def mergesort(list, p, r):
@@ -37,7 +47,7 @@ def mergesort(list, p, r):
 
 
 if __name__ == "__main__":
-    size = 1000000
+    size = 50000
     list = []
     for i in range(0, size):
         list.append(random.randint(0, size))
@@ -48,5 +58,6 @@ if __name__ == "__main__":
     print(end - start)
     start2 = time.time()
     second_list = sorted(second_list)
+    print(list)
     end2 = time.time()
     print(end2 - start2)
